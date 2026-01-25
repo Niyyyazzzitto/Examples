2b84aa3570f9e1362c020b4f1b4fb20486ba29d9
Жайсанбай Нияз <niyyyazzzitto@MacBook-Air-Zajsanbaj.local>
2026-01-25 23:14:16 +0500

    Add numbers examples

---
+++ Practice1/python_basics/Numbers.py
@@ -0,0 +1,18 @@
+import random
+
+q = 33
+print(type(q))
+
+a = 33.33
+print(type(a))
+
+z = 44j
+print(type(z))
+
+w = random.randrange(1,11)
+print(w)
+
+x = 4444
+c = float(x)
+print(c)
+print(type(c))
