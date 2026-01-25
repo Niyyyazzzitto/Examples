a866fcdf8f05722d7db89e48aefd1d0aa1403684
Жайсанбай Нияз <niyyyazzzitto@MacBook-Air-Zajsanbaj.local>
2026-01-25 23:30:54 +0500

    Add strings examples

---
+++ Practice1/python_basics/Strings.py
@@ -0,0 +1,19 @@
+print("Hi")
+print('Hi')
+
+print("He's name is 'Niyaz'")
+
+x = "error"
+print(x)
+
+a = "dedrfrftg"
+print(a[4])
+
+for x in a:
+    print(x)
+
+print(len(a))
+
+s = "Hello everyone"
+if "Hello" in s:
+    print("Yeeee!")
