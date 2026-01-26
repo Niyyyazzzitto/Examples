73235e31410df37f1f83ae61bc77d357e6457dae
Жайсанбай Нияз <niyyyazzzitto@MacBook-Air-Zajsanbaj.local>
2026-01-26 18:47:13 +0500

    Update Boolean_Values example

---
+++ Practice2/Boolean_Values.py
@@ -0,0 +1,22 @@
+print(15 > 9)
+print(2 == 2)
+print(13 >= 13)
+
+x = 3
+a = 4
+if x > a:
+    print("Yes!")
+else:
+    print("No!")
+
+print(bool(55))
+print(bool([]))
+print(bool("WD"))
+
+q = 1
+w = "F"
+print(bool(q))
+print(bool(w))
+
+z = 200.5
+print(isinstance(z, float))
