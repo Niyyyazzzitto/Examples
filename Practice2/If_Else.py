c8f2a0fbeadb6cb509c0ba0be29ed0d6a0bb180d
Жайсанбай Нияз <niyyyazzzitto@MacBook-Air-Zajsanbaj.local>
2026-01-27 12:19:08 +0500

    Add if_else examples

---
+++ Practice2/If_Else.py
@@ -0,0 +1,32 @@
+q = 43
+if q % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+a = "Niyaz"
+if len(a) > 0:
+    print(f"Hello, {a}")
+else:
+    print("GG")
+
+z = 20
+if 1 < z < 10:
+    print("Cool")
+else:
+    print("Bad")
+
+x = 100
+if x > q + 10:
+    print("d")
+else:
+    print("a")
+
+w = 200
+b = 33
+if b > w:
+  print("b is greater than a")
+elif w == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
