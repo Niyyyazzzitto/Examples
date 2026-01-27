fa2ad97dd843b44223dec9efc2fd29332303223c
Жайсанбай Нияз <niyyyazzzitto@MacBook-Air-Zajsanbaj.local>
2026-01-27 12:09:20 +0500

    Add if_statement examples

---
+++ Practice2/If_Statement.py
@@ -0,0 +1,19 @@
+age = 23
+if age >= 18:
+    print("You are an adult")
+
+x = True
+if x:
+    print("Yes")
+
+a = -3
+if a < 0:
+    print(f"{a} is a negative number")
+
+b = False
+if x or b:
+    print("True")
+
+s = 23
+if age == s:
+    print("Equal!")
