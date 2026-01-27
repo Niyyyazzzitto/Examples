69fa0c2c0848a1c992f19b6badba51e6170ef537
Жайсанбай Нияз <niyyyazzzitto@MacBook-Air-Zajsanbaj.local>
2026-01-27 12:01:08 +0500

    Add boolean_operators examples

---
+++ Practice2/Boolean_Operators.py
@@ -0,0 +1,20 @@
+print(not True and not False)
+print(False or True)
+
+x = "Hello world, my name is Niyaz"
+if "world" and "my" in x:
+    print("Yes!")
+
+a = True 
+b = False
+if a or b:
+    print("!!!")
+
+age = 18
+pr = True
+if age >= 18 and pr:
+    print("Cool!")
+
+r = False
+if not r:
+    print("!")
